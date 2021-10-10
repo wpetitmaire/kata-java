d2dbc6d68d9e0b33e792c2afd4ea6ed84b14518f
Willy PM <w.petitmaire@gmail.com>
2021-10-10 19:07:21 +0200

    Dupplication de l'ancienne classe + Ajout d'un test unitaire pour vérifier que le résultat est identique entre l'ancienne app et la nouvelle.

---
+++ src/test/java/com/gildedrose/OldGildedRoseTest.java
@@ -0,0 +1,56 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OldGildedRoseTest {
+
+    Item[] createItems() {
+        Item[] items = new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 3, 6) };
+
+        return items;
+    }
+
+    /**
+     * Comparaison entre l'ancienne application et la nouvelle pour s'assurer que rien n'est cassé pendant la
+     * refactorisation.
+     */
+    @Test
+     void compareOldAndNew() {
+
+        // Ancienne application
+        Item[] oldItems = createItems();
+        OldGildedRose oldApp = new OldGildedRose(oldItems);
+
+        // Nouvelle application
+        Item[] newItems = createItems();
+        GildedRose newApp = new GildedRose(newItems);
+
+        int days = 16;
+
+        for (int i = 0; i < days; i++) {
+
+            newApp.updateQuality();
+            oldApp.updateQuality();
+
+            // Comparaison de chaque item entre ancienne et nouvelle gestion
+            for (int j = 0; j < newItems.length; j++) {
+                assertEquals(newItems[j].sellIn, oldItems[j].sellIn);
+                assertEquals(newItems[j].quality, oldItems[j].quality);
+            }
+        }
+
+    }
+
+}
