30dd1e978766b439093ad449a591f848238f4065
Willy PM <w.petitmaire@gmail.com>
2021-10-11 22:11:05 +0200

    Création énumération des types d'articles + implémentation fonction de récupération du type d'article à partir du nom + Ajout TU associé

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,20 @@ class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    void getTypeArticle() {
+
+        Item[] items = new Item[] {
+            new Item("TEST", 0, 0),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        TypeArticle typeArticle = app.GetTypeArticle(items[0].name);
+        assertEquals(TypeArticle.DEFAULT, typeArticle);
+
+        typeArticle = app.GetTypeArticle(items[1].name);
+        assertEquals(TypeArticle.SULFURAS, typeArticle);
+    }
 }
