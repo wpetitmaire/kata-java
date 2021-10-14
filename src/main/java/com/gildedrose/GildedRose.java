d9b44396241ef9cd8e3c8ef8711f899c65770cc8
Willy PM <w.petitmaire@gmail.com>
2021-10-14 21:24:58 +0200

    Extraction des méthodes de GildedRose.updateQuality selon les différents types d'article. + Conversion "for" en Stream

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 class GildedRose {
     Item[] items;
 
@@ -9,98 +11,106 @@ class GildedRose {
 
     TypeArticle GetTypeArticle(String name) {
 
-        for(TypeArticle typeArticle: TypeArticle.values()) {
-
-            if(typeArticle.getNom().equals(name))
-                return typeArticle;
-        }
-
-        return TypeArticle.DEFAULT;
+        return Arrays.stream(TypeArticle.values())
+            .filter(typeArticle -> typeArticle.getNom().equals(name))
+            .findFirst()
+            .orElse(TypeArticle.DEFAULT);
     }
 
     public void updateQuality() {
 
-        System.out.println("--updateQuality--");
-
-        for (Item item : items) {
+        Arrays.stream(items).forEach(item -> {
 
             TypeArticle typeOfItem = this.GetTypeArticle(item.name);
 
             switch (typeOfItem) {
+
                 case DEFAULT:
-
-                    item.sellIn -= 1;
-
-                    // Si la qualité n'est pas encore nulle, elle se dégrade
-                    if(item.quality > 0) {
-
-                        item.quality -= 1;
-
-                        // Elle se dégrade doublement si l'article est périmé
-                        if(item.quality > 0 && item.sellIn < 0) {
-                            item.quality -= 1;
-                        }
-                    }
-
-                    continue;
+                    updateDefaultArticleQuality(item);
+                    return;
 
                 case AGED_BRIE:
-
-                    item.sellIn = item.sellIn - 1;
-
-                    // Si la qualité de l'article n'est pas au maximum, elle augmente
-                    if (item.quality < 50) {
-
-                        item.quality += 1;
-
-                        // Elle augmente doublement si l'article est périmé
-                        if(item.quality < 50 && item.sellIn < 0) {
-                            item.quality += 1;
-                        }
-                    }
-
-                    continue;
+                    updateAgedBrieArticleQuality(item);
+                    return;
 
                 case BACKSTAGE_PASSES:
-
-                    // Si la qualité n'est pas encore au maximum, elle augmente différemment selon le nombre le
-                    // nombre de jours de validité restants
-                    if (item.quality < 50) {
-
-                        // Moins de 5 jours restant
-                        if(item.sellIn <= 5 ) {
-                            item.quality += 3;
-                        }
-                        // Moins de 10 jours restant
-                        else if(item.sellIn <= 10 ) {
-                            item.quality += 2;
-                        }
-                        else {
-                            item.quality += 1;
-                        }
-
-                        if(item.quality > 50) {
-                            item.quality = 50;
-                        }
-                    }
-
-                    item.sellIn = item.sellIn - 1;
-
-                    // Une fois la date passée, la qualité tombe à 0.
-                    if(item.sellIn < 0 && item.quality > 0) {
-                        item.quality = 0;
-                    }
-
-                    continue;
+                    updateBackstagePassesQuality(item);
+                    return;
 
                 case SULFURAS:
+                    updateSulfurasArticleQuality(item);
+                    return;
+            }
+        });
+    }
 
-                    // La qualité est toujours de 80
-                    if(item.quality != 80) {
-                        item.quality = 80;
-                    }
+    private void updateSulfurasArticleQuality(Item item) {
 
-                    continue;
+        // La qualité est toujours de 80
+        if(item.quality != 80) {
+            item.quality = 80;
+        }
+    }
+
+    private void updateBackstagePassesQuality(Item item) {
+
+        // Si la qualité n'est pas encore au maximum, elle augmente différemment selon le nombre le
+        // nombre de jours de validité restants
+        if (item.quality < 50) {
+
+            // Moins de 5 jours restant
+            if(item.sellIn <= 5 ) {
+                item.quality += 3;
+            }
+            // Moins de 10 jours restant
+            else if(item.sellIn <= 10 ) {
+                item.quality += 2;
+            }
+            else {
+                item.quality += 1;
+            }
+
+            if(item.quality > 50) {
+                item.quality = 50;
+            }
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        // Une fois la date passée, la qualité tombe à 0.
+        if(item.sellIn < 0 && item.quality > 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void updateAgedBrieArticleQuality(Item item) {
+
+        item.sellIn = item.sellIn - 1;
+
+        // Si la qualité de l'article n'est pas au maximum, elle augmente
+        if (item.quality < 50) {
+
+            item.quality += 1;
+
+            // Elle augmente doublement si l'article est périmé
+            if(item.quality < 50 && item.sellIn < 0) {
+                item.quality += 1;
+            }
+        }
+    }
+
+    private void updateDefaultArticleQuality(Item item) {
+
+        item.sellIn -= 1;
+
+        // Si la qualité n'est pas encore nulle, elle se dégrade
+        if(item.quality > 0) {
+
+            item.quality -= 1;
+
+            // Elle se dégrade doublement si l'article est périmé
+            if(item.quality > 0 && item.sellIn < 0) {
+                item.quality -= 1;
             }
         }
     }
