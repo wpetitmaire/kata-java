906a85f8f2dd1a70cb6eb21ec4fa415f9477d4d8
Willy PM <w.petitmaire@gmail.com>
2021-10-14 22:31:00 +0200

    Ajout tests unitaires pour chaque type d'article

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -30,4 +30,69 @@ class GildedRoseTest {
         typeArticle = app.GetTypeArticle(items[1].name);
         assertEquals(TypeArticle.SULFURAS, typeArticle);
     }
+
+    @Test
+    void updateDefaultArticleQuality() {
+        Item[] items = new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 16; i++) {
+            app.updateQuality();
+        }
+
+        assertEquals(-6, items[0].sellIn);
+        assertEquals(0, items[0].quality);
+    }
+
+    @Test
+    void updateAgedBrieArticleQuality() {
+        Item[] items = new Item[] {
+            new Item("Aged Brie", 2, 0)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 16; i++) {
+            app.updateQuality();
+        }
+
+        assertEquals(-14, items[0].sellIn);
+        assertEquals(30, items[0].quality);
+    }
+
+    @Test
+    void updateBackstagePassesQuality() {
+        Item[] items = new Item[] {
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 16; i++) {
+            app.updateQuality();
+        }
+
+        assertEquals(-1, items[0].sellIn);
+        assertEquals(0, items[0].quality);
+    }
+
+    @Test
+    void updateSulfurasArticleQuality() {
+        Item[] items = new Item[] {
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 16; i++) {
+            app.updateQuality();
+        }
+
+        assertEquals(0, items[0].sellIn);
+        assertEquals(80, items[0].quality);
+    }
+
 }
