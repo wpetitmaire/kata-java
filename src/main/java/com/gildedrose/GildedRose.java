95c195592e7b14e79563e2ca4a1a59f994714800
Willy PM <w.petitmaire@gmail.com>
2021-10-17 20:54:16 +0200

    Création de constantes.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -51,8 +51,8 @@ class GildedRose {
     private void updateSulfurasArticleQuality(Item item) {
 
         // La qualité est toujours de 80
-        if(item.quality != 80) {
-            item.quality = 80;
+        if(item.quality != Item.SULFURAS_QUALITY) {
+            item.quality = Item.SULFURAS_QUALITY;
         }
     }
 
@@ -60,26 +60,26 @@ class GildedRose {
 
         // Si la qualité n'est pas encore au maximum, elle augmente différemment selon le nombre le
         // nombre de jours de validité restants
-        if (item.quality < 50) {
+        if (item.quality < Item.MAX_QUALITY) {
 
             // Moins de 5 jours restant
             if(item.sellIn <= 5 ) {
-                item.quality += 3;
+                item.quality += Item.BACKSTAGE_QUALITY_STEP_LESS_5_DAYS;
             }
             // Moins de 10 jours restant
             else if(item.sellIn <= 10 ) {
-                item.quality += 2;
+                item.quality += Item.BACKSTAGE_QUALITY_STEP_LESS_10_DAYS;
             }
             else {
-                item.quality += 1;
+                item.quality += Item.QUALITY_STEP;
             }
 
-            if(item.quality > 50) {
-                item.quality = 50;
+            if(item.quality > Item.MAX_QUALITY) {
+                item.quality = Item.MAX_QUALITY;
             }
         }
 
-        item.sellIn = item.sellIn - 1;
+        item.sellIn -= Item.SELLIN_STEP;
 
         // Une fois la date passée, la qualité tombe à 0.
         if(item.sellIn < 0 && item.quality > 0) {
@@ -89,32 +89,32 @@ class GildedRose {
 
     private void updateAgedBrieArticleQuality(Item item) {
 
-        item.sellIn = item.sellIn - 1;
+        item.sellIn -= Item.SELLIN_STEP;
 
         // Si la qualité de l'article n'est pas au maximum, elle augmente
-        if (item.quality < 50) {
+        if (item.quality < Item.MAX_QUALITY) {
 
-            item.quality += 1;
+            item.quality += Item.QUALITY_STEP;
 
             // Elle augmente doublement si l'article est périmé
-            if(item.quality < 50 && item.sellIn < 0) {
-                item.quality += 1;
+            if(item.quality < Item.MAX_QUALITY && item.sellIn < 0) {
+                item.quality += Item.QUALITY_STEP;
             }
         }
     }
 
     private void updateDefaultArticleQuality(Item item) {
 
-        item.sellIn -= 1;
+        item.sellIn -= Item.SELLIN_STEP;
 
         // Si la qualité n'est pas encore nulle, elle se dégrade
         if(item.quality > 0) {
 
-            item.quality -= 1;
+            item.quality -= Item.QUALITY_STEP;
 
             // Elle se dégrade doublement si l'article est périmé
             if(item.quality > 0 && item.sellIn < 0) {
-                item.quality -= 1;
+                item.quality -= Item.QUALITY_STEP;
             }
         }
     }
