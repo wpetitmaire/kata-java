85921da071cf8b03a0b6376276e0f403380cc432
Willy PM <w.petitmaire@gmail.com>
2021-10-31 17:43:17 +0100

    Changement pour les spécification propres au type BackstagePass de type item vers type BackstagePass.

--- src/main/java/com/gildedrose/specifications/backstagepass/AvailabilityLessOrEqual10DaysSpecification.java
+++ src/main/java/com/gildedrose/specifications/backstagepass/AvailabilityLessOrEqual10DaysSpecification.java
@@ -1,11 +1,12 @@
 package com.gildedrose.specifications.backstagepass;
 
 import com.gildedrose.Item;
+import com.gildedrose.items.BackstagePass;
 import com.pattern.AbstractSpecification;
 
-public class AvailabilityLessOrEqual10DaysSpecification extends AbstractSpecification<Item> {
+public class AvailabilityLessOrEqual10DaysSpecification extends AbstractSpecification<BackstagePass> {
     @Override
-    public boolean isSatisfiedBy(Item item) {
+    public boolean isSatisfiedBy(BackstagePass item) {
         return item.sellIn <= 10;
     }
 }
--- src/main/java/com/gildedrose/specifications/backstagepass/AvailabilityLessOrEqual5DaysSpecification.java
+++ src/main/java/com/gildedrose/specifications/backstagepass/AvailabilityLessOrEqual5DaysSpecification.java
@@ -1,12 +1,13 @@
 package com.gildedrose.specifications.backstagepass;
 
 import com.gildedrose.Item;
+import com.gildedrose.items.BackstagePass;
 import com.pattern.AbstractSpecification;
 
-public class AvailabilityLessOrEqual5DaysSpecification extends AbstractSpecification<Item> {
+public class AvailabilityLessOrEqual5DaysSpecification extends AbstractSpecification<BackstagePass> {
 
     @Override
-    public boolean isSatisfiedBy(Item item) {
+    public boolean isSatisfiedBy(BackstagePass item) {
         return item.sellIn <= 5;
     }
 }
